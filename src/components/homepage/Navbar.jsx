de0000809d52c7d3f086f56df34fea2a639f7ab7
butong <anggi21.af@gmail.com>
2023-12-14 12:29:59 +0700

    add: login button

--- src/components/homepage/Navbar.jsx
+++ src/components/homepage/Navbar.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { Logo, close, menu } from '@/assets';
+import Button from '../Button';
 import { navLinks } from '@/constants';
 import styles from '@/style';
+import { useAuth } from '@/store/auth';
 
 const Navbar = () => {
   const [active, setActive] = useState('Home');
   const [toggle, setToggle] = useState(false);
+  const { isLogin } = useAuth();
 
   return (
     <div className='bg-primary w-full overflow-hidden'>
@@ -24,7 +27,8 @@ const Navbar = () => {
                   className={`font-poppins font-normal cursor-pointer text-[16px] ${
                     active === nav.title ? 'text-white' : 'text-dimWhite'
                   } ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
-                  onClick={() => setActive(nav.title)}>
+                  onClick={() => setActive(nav.title)}
+                >
                   <NavLink to={`${nav.id}`}>{nav.title}</NavLink>
                 </li>
               ))}
@@ -41,7 +45,8 @@ const Navbar = () => {
               <div
                 className={`${
                   !toggle ? 'hidden' : 'flex'
-                } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+              >
                 <ul className='list-none flex justify-end items-start flex-1 flex-col'>
                   {navLinks.map((nav, index) => (
                     <li
@@ -49,13 +54,23 @@ const Navbar = () => {
                       className={`font-poppins font-medium cursor-pointer text-[16px] ${
                         active === nav.title ? 'text-white' : 'text-dimWhite'
                       } ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}
-                      onClick={() => setActive(nav.title)}>
+                      onClick={() => setActive(nav.title)}
+                    >
                       <a href={`#${nav.id}`}>{nav.title}</a>
                     </li>
                   ))}
                 </ul>
               </div>
             </div>
+
+            {/* Kondisional rendering untuk button login */}
+            {!isLogin && (
+              <div>
+                <Link to='/login'>
+                  <Button styles='ml-4' name='Login' />
+                </Link>
+              </div>
+            )}
           </nav>
         </div>
       </div>
