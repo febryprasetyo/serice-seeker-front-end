2f3c4652c60fe16ceaa10ce2a2aa13d60f0c1bae
butong <anggi21.af@gmail.com>
2023-12-14 12:29:16 +0700

    add: protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import PropTypes from 'prop-types';
 import { Navbar } from '@/components';
 import Homepage from '@/page/Homepage';
 import Login from '@/page/auth/Login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from '@/page/auth/Signup';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useAuth } from '@/store/auth';
+
+function ProtectedSignupRoute({ element }) {
+  const { isLogin } = useAuth();
+
+  if (isLogin) {
+    return <Navigate to="/" />;
+  }
+
+  return element;
+}
+
+ProtectedSignupRoute.propTypes = {
+  element: PropTypes.element.isRequired,
+};
 
 function App() {
   return (
@@ -11,7 +28,14 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage />}></Route>
           <Route path='/homepage' element={<Homepage />}></Route>
-          <Route path='/login' element={<Login />}></Route>
+          <Route
+            path='/login'
+            element={<Login />}
+          ></Route>
+          <Route
+            path='/signup'
+            element={<ProtectedSignupRoute element={<Signup />} />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </>
