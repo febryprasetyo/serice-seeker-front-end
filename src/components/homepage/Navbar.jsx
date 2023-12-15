4d9912c4490ce52833996a3b81c4c4af7a483bd6
butong <anggi21.af@gmail.com>
2023-12-16 05:03:56 +0700

    change navbar components

--- src/components/homepage/Navbar.jsx
+++ src/components/homepage/Navbar.jsx
@@ -1,32 +1,90 @@
-// Your Navbar component
-import { useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { Logo, close, menu } from '@/assets';
+import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Logo, close, menu, defaultUserProfileImage } from '@/assets';
 import Button from '../Button';
 import { navLinks } from '@/constants';
 import styles from '@/style';
 import { useAuth } from '@/store/auth';
+import { getUserProfile } from '../../api/api'; // Update the path based on your actual path
 
 const Navbar = () => {
+  // State variables
   const [active, setActive] = useState('Home');
   const [toggle, setToggle] = useState(false);
-  const { isLogin } = useAuth();
+  const { isLogin, user, logOut } = useAuth();
+  const [userProfile, setUserProfile] = useState(null);
 
-  // Filter out login and signup links only when the user is logged in
-  const filteredNavLinks = isLogin
-    ? navLinks.filter((nav) => nav.id !== 'login' && nav.id !== 'signup')
-    : navLinks;
+  // Fetch user profile on login
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const profileData = await getUserProfile();
+        setUserProfile(profileData);
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    };
+
+    if (isLogin) {
+      fetchUserProfile();
+    }
+  }, [isLogin]);
+
+ // Filter out login and signup links only when the user is logged in
+ const filteredNavLinks = isLogin
+ ? navLinks.filter((nav) => nav.id !== 'login' && nav.id !== 'signup')
+ : navLinks;
+
+ // Determine profile image
+  const profileImage = userProfile?.data.profileImage || defaultUserProfileImage;
+
+  // Logout handler
+  const handleLogout = () => {
+    logOut();
+    setToggle(true);
+  };
+  console.log()
+  // Render user menu
+  const renderUserMenu = () => (
+    <div className={`relative ${styles.cursorPointer} ml-auto`}>
+      <div
+        className="flex items-center gap-4 cursor-pointer"
+        onClick={() => setToggle(!toggle)}
+      >
+        {userProfile ? (
+          <img src={profileImage} alt="Profile" className="w-12 h-12 object-cover rounded-full" />
+        ) : (
+          <img src={defaultUserProfileImage} alt="Default Profile" className="w-8 h-8 object-cover rounded-full" />
+        )}
+        {/* { <span className="text-white font-medium">{userProfile?.data.username}</span> } */}
+      </div>
+
+      {toggle && (
+        <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
+          <NavLink to="/profile">
+            <div className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Profile</div>
+          </NavLink>
+          <div
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer"
+            onClick={handleLogout}
+          >
+            Logout
+          </div>
+        </div>
+      )}
+    </div>
+  );
 
   return (
-    <div className='bg-primary w-full overflow-hidden'>
+    <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          <nav className='w-full flex py-6 justify-between items-center navbar'>
-            <NavLink to='/'>
-              <img src={Logo} alt='hoobank' className='w-[124px] ' />
+          <nav className="w-full flex py-6 justify-between items-center navbar">
+            <NavLink to="/">
+              <img src={Logo} alt="hoobank" className="w-[124px] " />
             </NavLink>
 
-            <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+            <ul className="list-none sm:flex hidden justify-end items-center flex-1">
               {filteredNavLinks.map((nav, index) => (
                 <li
                   key={nav.id}
@@ -35,16 +93,17 @@ const Navbar = () => {
                   } ${index === filteredNavLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
                   onClick={() => setActive(nav.title)}
                 >
-                  <NavLink to={`${nav.id}`}>{nav.title}</NavLink>
+                  <NavLink to={`/${nav.id}`}>{nav.title}</NavLink>
                 </li>
               ))}
+              {isLogin && <li className="ml-10">{renderUserMenu()}</li>}
             </ul>
 
-            <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <div className="sm:hidden flex flex-1 justify-end items-center">
               <img
                 src={toggle ? close : menu}
-                alt='menu'
-                className='w-[28px] h-[28px] object-contain'
+                alt="menu"
+                className="w-[28px] h-[28px] object-contain"
                 onClick={() => setToggle(!toggle)}
               />
 
@@ -53,7 +112,8 @@ const Navbar = () => {
                   !toggle ? 'hidden' : 'flex'
                 } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
               >
-                <ul className='list-none flex justify-end items-start flex-1 flex-col'>
+                <ul className="list-none flex justify-end items-start flex-1 flex-col">
+                  {isLogin && <li className="mb-4">{renderUserMenu()}</li>}
                   {filteredNavLinks.map((nav, index) => (
                     <li
                       key={nav.id}
