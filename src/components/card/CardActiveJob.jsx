05caf03ea63cc3af0cc2ce628d529586a6e82527
butong <anggi21.af@gmail.com>
2023-12-19 04:10:24 +0700

    change layout

--- src/components/card/CardActiveJob.jsx
+++ src/components/card/CardActiveJob.jsx
@@ -2,25 +2,23 @@ import { Card, CardBody, Typography } from '@material-tailwind/react';
 
 const CardActiveJob = ({ title, status }) => {
   return (
-    <div>
-      <Card className='w-70'>
-        <div className='h-20 flex mt-4  justify-center'>
-          <img
-            className='h-20 w-20 rounded-full object-cover object-center'
-            src='https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80'
-            alt='nature image'
-          />
-        </div>
-        <CardBody className='text-center'>
-          <Typography variant='h5' color='blue-gray' className='mb-2'>
-            {title}
-          </Typography>
-          <Typography color='blue-gray' className='font-medium' textGradient>
-            {status}
-          </Typography>
-        </CardBody>
-      </Card>
-    </div>
+    <Card className='w-48 p-4 m-2'>
+      <div className='h-20 flex mt-4 justify-center'>
+        <img
+          className='h-20 w-20 rounded-full object-cover object-center'
+          src='https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80'
+          alt='nature image'
+        />
+      </div>
+      <CardBody className='text-center'>
+        <Typography variant='h5' color='blue-gray' className='mb-2'>
+          {title}
+        </Typography>
+        <Typography color='blue-gray' className='font-medium' textGradient>
+          {status}
+        </Typography>
+      </CardBody>
+    </Card>
   );
 };
 
