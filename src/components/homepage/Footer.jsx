fd4fbb609476589264ff572be41340f97eee7cc5
butong <anggi21.af@gmail.com>
2023-12-19 04:11:20 +0700

    add style

--- src/components/homepage/Footer.jsx
+++ src/components/homepage/Footer.jsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
+import styles from '@/style';
 
 const footer = () => {
   return (
         <div className="bg-primary w-full overflow-hidden text-white p-4 text-center ">
-          <nav className="flex justify-center space-x-4 mb-4">
-            <Link to="/about-us">About Us</Link>
-            <Link to="/faq">FAQ</Link>
-            <Link to="/contact-us">Contact Us</Link>
-            <Link to="/term-of-service">Term of Service</Link>
-            <Link to="/privacy-policy">Privacy Policy</Link>
-          </nav>
-          <p>&copy; 2023 Service Seeker. All rights reserved.</p>
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+              <nav className="flex justify-center space-x-4 mb-4">
+                <Link to="/about-us">About Us</Link>
+                <Link to="/faq">FAQ</Link>
+                <Link to="/contact-us">Contact Us</Link>
+                <Link to="/term-of-service">Term of Service</Link>
+                <Link to="/privacy-policy">Privacy Policy</Link>
+              </nav>
+              <p>&copy; 2023 Service Seeker. All rights reserved.</p>
+            </div>
+          </div>
         </div>
   );
 };
